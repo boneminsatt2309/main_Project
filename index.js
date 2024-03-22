f109c394c5b7f08d2ecc220d16296370f7d77110
boneminsatt2309 <76002536+boneminsatt2309@users.noreply.github.com>
2024-03-22 13:18:51 +0630

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// var myName = prompt("What is your name?")
+// var myName= "PPPP"; 
+// alert("You Name is " +myName)
+// console.log("Hello WOrld")
+
+// function test(){
+//     var a="3";
+//     var b="8";
+//     var temp = a;
+//     a = b;
+//     b = temp;
+//     console.log("a is "+a)
+//     console.log("b is "+b)
+// }
+
+// var message = "Hello";
+// var name = "World";
+// alert(message + " there, " + name);
+
+var word = prompt("Type anything as you can.")
+var length = word.length;
+alert("You have written " +length+ " words.")
